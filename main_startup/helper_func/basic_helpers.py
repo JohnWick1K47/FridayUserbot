1bdd30ad863a47083352e6f8311588c17ac331a4
JohnWick1K47 <69172674+JohnWick1K47@users.noreply.github.com>
2021-06-25 08:55:47 +0530

    Update basic_helpers.py

--- main_startup/helper_func/basic_helpers.py
+++ main_startup/helper_func/basic_helpers.py
@@ -194,7 +194,7 @@ def cb_wrapper(func):
         users = await get_all_pros()
         if cb.from_user.id not in users:
             await cb.answer(
-                "You Can't Access Me, Only My Master Can. Why Don't You Deploy Your Own Friday? - @FridayOT",
+                "You Can't Access Me, Only My Master Can. - @HKFlixOfficialBot",
                 cache_time=0,
                 show_alert=True,
             )
